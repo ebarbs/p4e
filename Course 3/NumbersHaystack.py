ad123cb3db0749abadad9552a50263d7cea9d06e
barbs <barbieri.eric@gmail.com>
2017-11-19 15:40:02 -0500

    Assignment 11.2 Passed

--- Course 3/NumbersHaystack.py
+++ Course 3/NumbersHaystack.py
@@ -4,8 +4,8 @@
 # the file and compute the sum of the numbers.
 #
 # Data Files
-# We provide two files for this assignment. One is a sample file where we give you the sum for your testing and the other
-# is the actual data you need to process for the assignment.
+# We provide two files for this assignment. One is a sample file where we give you the sum for your testing and the
+#  other is the actual data you need to process for the assignment.
 #
 # Sample data: http://py4e-data.dr-chuck.net/regex_sum_42.txt (There are 90 values with a sum=445833)
 # Actual data: http://py4e-data.dr-chuck.net/regex_sum_43683.txt (There are 79 values and the sum ends with 321)
@@ -13,7 +13,21 @@
 # program. Note: Each student will have a distinct data file for the assignment - so only use your own data file for
 # analysis.
 # Data Format
-# The file contains much of the text from the introduction of the textbook except that random numbers are inserted
-# throughout the text. Here is a sample of the output you might see:
 
+import re
+
+name = input("Enter file:")
+if len(name) < 1 : name = "regex_sum_43683.txt"
+handle = open(name)
+
+num_sum = 0
+
+for line in handle:
+    y = re.findall("[0-9]+", line)
+    for x in y:
+        if not x == None:
+            tmp = int(x)
+            num_sum = num_sum + tmp
+
+print (num_sum)
 
